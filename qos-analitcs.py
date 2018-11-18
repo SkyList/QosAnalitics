ea481eb8f27b774775c672b5ab9122d1a1c62a54
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2018-11-17 22:15:01 -0300

    .

--- qos-analitcs.py
+++ qos-analitcs.py
@@ -1,7 +1,9 @@
-import re
-import sys
-import subprocess
 from threading import Thread
+import subprocess
+import sys
+import csv
+import re
+import os
 
 def getPingResults(host, nPackages):
     process = subprocess.Popen(['ping', str(host), '-c', str(nPackages)], stdout=subprocess.PIPE)
@@ -16,9 +18,21 @@ def getSpeedTest():
     out, err = process.communicate()
     return re.findall(r"(\d+\.\d+)", out)
 
+def saveCsv(fileNAme):
+
+
 def __init(args):
     print('Initializing...')
     print ('rtt values: ',getPingResults(args[1], 4))
     print ('speedtest: ',getSpeedTest())
 
 __init(sys.argv)
+
+import csv
+import os
+
+os.system('touch results.csv')
+
+with open('results.csv', 'a') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    spamwriter.writerow(['min', 'avg', 'max', 'mdev'])
